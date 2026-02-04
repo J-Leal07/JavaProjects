0ab979d047f5bb46f374dfbfcc3ac164749eb674
Leal <joaovictorm.leal07@gmail.com>
2026-02-03 21:37:34 -0300

    Feat conversorMedidas.java
    feat: adiciona loop while para execução contínua.

--- conversorMedidas.java
+++ conversorMedidas.java
@@ -1,7 +1,8 @@
 package projetoJava1;
+
 import java.util.Scanner;
 
-public class program {
+public class conversorMedidas {
 
 	void main() {
 
@@ -9,27 +10,22 @@ public class program {
 
 		System.out.println("Seja bem-vindo(a)!");
 		System.out.println("Operações Disponíveis:");
-		System.out.println("");
+		System.out.println();
 		System.out.println("(1) Real para Dólar;");
 		System.out.println("(2) Celsius para Fahrenheit;");
 		System.out.println("(3) Metro para Foot.");
-		System.out.println("");
+		System.out.println();
 		System.out.println("Deseja continuar? (Sim ou Não)");
-		
 		String confirm = sc.next();
-		
-		if (confirm.equalsIgnoreCase("Sim")) {
-			
+		while (confirm.equals("Sim")) {
 			System.out.println("Obrigado por continuar conosco, por favor, digite o número da operação desejada.");
 			int op = sc.nextInt();
-			
 			switch (op) {
 			case 1:
 				System.out.println("Digite a quantidade em reais:");
 				double real = sc.nextDouble();
 				double dolar = real / 5.16;
 				System.out.println("Resultado: " + dolar);
-				
 				break;
 			case 2:
 				System.out.println("Digite a quantidade em graus:");
@@ -40,16 +36,14 @@ public class program {
 			case 3:
 				System.out.println("Digite a quantidade em metros:");
 				double meters = sc.nextDouble();
-				double foot = meters * 3.2;
+				double foot = meters * 3.28;
 				System.out.println("Resultado aproximado: " + foot);
 				break;
-			}	
-		} 
-		
-		else {
-			System.out.println("Quando quiser ajuda, pode chamar!");
+			}
+			System.out.println("Deseja continuar conosco?");
+			confirm = sc.next();
 		}
-		
+		System.out.println("Quando quiser ajuda, pode chamar!");
 		sc.close();
 
 	}
